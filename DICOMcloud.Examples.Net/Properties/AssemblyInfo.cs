dffbd488c12a217974cffb5d62e95e8bee8752ec
Zaid Safadi <zsafadi@hotmail.com>
2017-11-15 21:43:01 -0500

    Update Assembly info

--- DICOMcloud.Examples.Net/Properties/AssemblyInfo.cs
+++ DICOMcloud.Examples.Net/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DICOMcloud.Examples")]
+[assembly: AssemblyTitle("DICOMcloud.Examples.Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DICOMcloud.Examples")]
+[assembly: AssemblyProduct("DICOMcloud HQ.")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
